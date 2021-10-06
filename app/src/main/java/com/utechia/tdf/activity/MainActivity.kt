6463a5452e3f996f1f2a0bd1c13a6e27ee71b88f
abbaslotfinasab <abbas.lotfinasab@utechia.com>
2021-10-06 16:04:17 +0330

    bottom navigation

--- app/src/main/java/com/utechia/tdf/activity/MainActivity.kt
+++ app/src/main/java/com/utechia/tdf/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.utechia.tdf.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.view.ViewCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,10 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val button:ImageButton = findViewById(R.id.customButton)
+        ViewCompat.setTranslationZ(button, 1F)
+        button.bringToFront()
         val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
 
     }
+
 }
