d362804986f2636f67141ad7938df92892d70619
abbaslotfinasab <abbas.lotfinasab@utechia.com>
2021-10-07 12:14:59 +0330

    navigation drawer

--- app/src/main/java/com/utechia/tdf/activity/MainActivity.kt
+++ app/src/main/java/com/utechia/tdf/activity/MainActivity.kt
@@ -2,47 +2,46 @@ package com.utechia.tdf.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.utechia.tdf.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(){
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationView:NavigationView = findViewById(R.id.navigationView)
         val drawerLayout:DrawerLayout = findViewById(R.id.drawer_layout)
         val imageView:ImageView = findViewById(R.id.menu)
-        val exitImage:ImageView = findViewById(R.id.exit)
         val button:ImageButton = findViewById(R.id.customButton)
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         bottomNavigationView.setupWithNavController(navController)
-
-        ViewCompat.setTranslationZ(button, 1F)
         button.bringToFront()
+
 
         imageView.setOnClickListener {
 
             drawerLayout.openDrawer(GravityCompat.START)
-            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_OPEN)
-
-        }
-
-        exitImage.setOnClickListener {
-            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            drawerLayout.closeDrawer(GravityCompat.START)
 
         }
 
