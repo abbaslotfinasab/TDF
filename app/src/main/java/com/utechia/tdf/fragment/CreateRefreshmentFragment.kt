fe9bb23281ffaa12b3022e2ae31049b4dacc51f2
abbaslotfinasab <abbas.lotfinasab@utechia.com>
2021-10-21 16:54:02 +0330

    create refreshment

---
+++ app/src/main/java/com/utechia/tdf/fragment/CreateRefreshmentFragment.kt
@@ -0,0 +1,125 @@
+package com.utechia.tdf.fragment
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.utechia.tdf.databinding.FragmentCreateRefreshmentBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CreateRefreshmentFragment : Fragment() {
+
+    private lateinit var binding: FragmentCreateRefreshmentBinding
+    private var kind = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCreateRefreshmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (arguments!=null)
+            kind = requireArguments().getInt("kind", 0)
+
+        select(kind)
+
+
+        binding.food.setOnClickListener {
+
+            unselect()
+            select(1)
+
+
+        }
+
+        binding.drink.setOnClickListener {
+
+            unselect()
+            select(2)
+
+
+        }
+
+
+        binding.favorite.setOnClickListener {
+
+            unselect()
+            select(3)
+
+        }
+
+
+        binding.order.setOnClickListener {
+
+            unselect()
+            select(4)
+
+
+
+        }
+
+    }
+
+    private fun select(kind:Int){
+
+        when(kind){
+
+            1 ->{
+
+                unselect()
+                binding.food.setBackgroundColor(Color.parseColor("#335DE0"))
+                binding.foodText.setTextColor(Color.WHITE)
+                binding.title.text = binding.foodText.text.toString()
+
+            }
+
+            2 ->{
+                unselect()
+                binding.drink.setBackgroundColor(Color.parseColor("#335DE0"))
+                binding.drinkText.setTextColor(Color.WHITE)
+                binding.title.text = binding.drinkText.text.toString()
+            }
+
+            3 ->{
+                unselect()
+                binding.favorite.setBackgroundColor(Color.parseColor("#335DE0"))
+                binding.favoriteText.setTextColor(Color.WHITE)
+                binding.title.text = binding.favoriteText.text.toString()
+
+            }
+
+            4 ->{
+                unselect()
+                binding.order.setBackgroundColor(Color.parseColor("#335DE0"))
+                binding.orderText.setTextColor(Color.WHITE)
+                binding.title.text = binding.orderText.text.toString()
+            }
+
+
+        }
+
+
+    }
+
+    private fun unselect() {
+
+        binding.food.setBackgroundColor(Color.parseColor("#FCFCFD"))
+        binding.drink.setBackgroundColor(Color.parseColor("#FCFCFD"))
+        binding.order.setBackgroundColor(Color.parseColor("#FCFCFD"))
+        binding.favorite.setBackgroundColor(Color.parseColor("#FCFCFD"))
+        binding.foodText.setTextColor(Color.BLACK)
+        binding.drinkText.setTextColor(Color.BLACK)
+        binding.orderText.setTextColor(Color.BLACK)
+        binding.favoriteText.setTextColor(Color.BLACK)
+
+    }
+
+}
--- app/src/main/java/com/utechia/tdf/fragment/TeaBoyFragment.kt
+++ app/src/main/java/com/utechia/tdf/fragment/TeaBoyFragment.kt
@@ -1,12 +1,12 @@
 package com.utechia.tdf.fragment
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.utechia.tdf.R
 import com.utechia.tdf.databinding.FragmentTeaBoyBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,22 +29,31 @@ class TeaBoyFragment : Fragment() {
 
         binding.food.setOnClickListener {
 
+            val bundle = bundleOf("kind" to 1)
+            findNavController().navigate(R.id.action_teaBoyFragment_to_createRefreshmentFragment,bundle)
+
         }
 
         binding.drink.setOnClickListener {
+
+            val bundle = bundleOf("kind" to 2)
+            findNavController().navigate(R.id.action_teaBoyFragment_to_createRefreshmentFragment,bundle)
 
         }
 
         binding.favorite.setOnClickListener {
 
+            val bundle = bundleOf("kind" to 3)
+            findNavController().navigate(R.id.action_teaBoyFragment_to_createRefreshmentFragment,bundle)
+
         }
 
         binding.order.setOnClickListener {
 
+            val bundle = bundleOf("kind" to 4)
+            findNavController().navigate(R.id.action_teaBoyFragment_to_createRefreshmentFragment,bundle)
+
         }
-
-
-
 
     }
 
