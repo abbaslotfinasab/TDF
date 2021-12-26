c6313c284a9a0c81d816fbe23a94cf6c66a533fc
Abbas.lotfinasab <Abbas.lotfinasab@utechia.com>
2021-12-26 12:52:05 +0330

    ticket design

--- app/src/main/java/com/utechia/tdf/more/CreateTicketFragment.kt
+++ app/src/main/java/com/utechia/tdf/more/CreateTicketFragment.kt
@@ -24,6 +24,8 @@ class CreateTicketFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
     }
 
 }
