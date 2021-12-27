d2a81f6c97f0d05716cbb820aa9a65d553f1ee83
Abbas.lotfinasab <Abbas.lotfinasab@utechia.com>
2021-12-27 22:53:53 +0330

    ticket upload file

--- app/src/main/java/com/utechia/tdf/ticket/CreateTicketFragment.kt
+++ app/src/main/java/com/utechia/tdf/ticket/CreateTicketFragment.kt
@@ -1,12 +1,17 @@
 package com.utechia.tdf.ticket
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.github.dhaval2404.imagepicker.ImagePicker
 import com.utechia.tdf.R
 import com.utechia.tdf.databinding.FragmentCreateTicketBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class CreateTicketFragment : Fragment() {
 
     private lateinit var binding: FragmentCreateTicketBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +45,53 @@ class CreateTicketFragment : Fragment() {
             findNavController().navigate(R.id.action_createTicketFragment_to_ticketConfirmationFragment)
         }
 
+        binding.btnUpload.setOnClickListener {
+
+            findNavController().navigate(R.id.action_createTicketFragment_to_uploadFragment)
+
+
+        }
     }
 
+    fun openGallery(){
+
+        ImagePicker.with(this)
+            .crop()	    			//Crop image(Optional), Check Customization for more option
+            .compress(2048)			//Final image size will be less than 1 MB(Optional)
+            .maxResultSize(1080, 1080) //Final image resolution will be less than 1080 x 1080(Optional)
+            .galleryOnly()
+            .start()
+
+    }
+
+    fun openCamera(){
+
+        ImagePicker.with(this)
+            .crop()	    			//Crop image(Optional), Check Customization for more option
+            .compress(2048)			//Final image size will be less than 1 MB(Optional)
+            .maxResultSize(1080, 1080) //Final image resolution will be less than 1080 x 1080(Optional)
+            .cameraOnly()
+            .start()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                //Image Uri will not be null for RESULT_OK
+                val uri: Uri = data?.data!!
+
+                // Use Uri object instead of File to avoid storage permissions
+                Toast.makeText(context, "ok", Toast.LENGTH_SHORT).show()
+
+            }
+            ImagePicker.RESULT_ERROR -> {
+                Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                Toast.makeText(context, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/utechia/tdf/ticket/UploadFragment.kt
@@ -0,0 +1,57 @@
+package com.utechia.tdf.ticket
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.utechia.tdf.R
+import com.utechia.tdf.databinding.FragmentUploadBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+
+@AndroidEntryPoint
+class UploadFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentUploadBinding
+    private lateinit var navHostFragment :NavHostFragment
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUploadBinding.inflate(inflater, container, false)
+
+        if(dialog !=null && dialog?.window !=null){
+            dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        }
+        return binding.root
+    }
+
+    override fun getTheme(): Int = R.style.AppBottomSheetDialogTheme
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navHostFragment = requireActivity().supportFragmentManager.fragments[0] as NavHostFragment
+        val parent = navHostFragment.childFragmentManager.primaryNavigationFragment as CreateTicketFragment
+
+        binding.camera.setOnClickListener {
+            parent.openCamera()
+            dialog?.dismiss()
+        }
+
+        binding.gallery.setOnClickListener {
+            parent.openGallery()
+            dialog?.dismiss()
+        }
+
+    }
+}
