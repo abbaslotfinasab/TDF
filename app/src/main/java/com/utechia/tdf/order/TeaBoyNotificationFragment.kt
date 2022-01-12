0def9571fb00d96e4149ff332947bbc4f3272a53
abbbas <abbas.lotfinasab@utechia.com>
2022-01-12 15:26:23 +0330

    push notification

--- app/src/main/java/com/utechia/tdf/order/TeaBoyNotificationFragment.kt
+++ app/src/main/java/com/utechia/tdf/order/TeaBoyNotificationFragment.kt
@@ -16,14 +16,19 @@ import com.utechia.domain.utile.Result
 import com.utechia.tdf.R
 import com.utechia.tdf.databinding.FragmentNotificationTeaboyBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class TeaBoyNotificationFragment : DialogFragment() {
 
     private lateinit var binding: FragmentNotificationTeaboyBinding
     private val orderAdapter:NotificationAdapter = NotificationAdapter()
-    private val userOrderViewModel:UserOrderViewModel by viewModels()
+    private val teaOrderViewModel:TeaBoyOrderViewModel by viewModels()
     private var cartId = 0
+    var sdf: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+    private lateinit var dateFormat: Date
+    private var simple = ""
 
 
     override fun onCreateView(
@@ -46,7 +51,7 @@ class TeaBoyNotificationFragment : DialogFragment() {
         if (arguments != null)
             cartId = requireArguments().getInt("cartId", 0)
 
-        userOrderViewModel.singleOrder(cartId)
+        teaOrderViewModel.singleOrderTeaBoy(cartId)
 
         binding.notificationRecycler.apply {
             adapter = orderAdapter
@@ -67,23 +72,53 @@ class TeaBoyNotificationFragment : DialogFragment() {
     }
 
     private fun observer() {
-        userOrderViewModel.userOrderModel.observe(viewLifecycleOwner){
+        teaOrderViewModel.userOrderModel.observe(viewLifecycleOwner){
 
 
             when (it) {
                 is Result.Success -> {
                     binding.prg.visibility = View.GONE
+                    binding.btnAccept.visibility = View.VISIBLE
+                    binding.exit.visibility = View.VISIBLE
+                    binding.imageView4.visibility = View.VISIBLE
+                    binding.textview1.visibility = View.VISIBLE
                     binding.notificationRecycler.visibility = View.VISIBLE
+                    binding.imageView13.visibility = View.VISIBLE
+                    binding.imageView12.visibility = View.VISIBLE
+                    binding.username.visibility = View.VISIBLE
+                    binding.subtitle.visibility = View.VISIBLE
+                    binding.date.visibility = View.VISIBLE
+                    binding.order.visibility = View.VISIBLE
+                    binding.username.text = it.data[0].user?.displayName
+                    binding.location.text = it.data[0].user?.officeWorkStation
+                    binding.order.text = it.data[0].id.toString()
+                    dateFormat = sdf.parse(it.data[0].updatedAt!!)!!
+                    simple = SimpleDateFormat("yyyy.MM.dd | HH:mm", Locale.getDefault()).format(dateFormat)
+                    binding.date.text = "$simple"
                     orderAdapter.addData(it.data[0].cart?.items!!)
                 }
 
                 is Result.Loading -> {
                     binding.prg.visibility = View.VISIBLE
+                    binding.btnAccept.visibility = View.INVISIBLE
+                    binding.exit.visibility = View.INVISIBLE
+                    binding.imageView4.visibility = View.INVISIBLE
+                    binding.textview1.visibility = View.INVISIBLE
+                    binding.notificationRecycler.visibility = View.INVISIBLE
+                    binding.imageView13.visibility = View.INVISIBLE
+                    binding.imageView12.visibility = View.INVISIBLE
+                    binding.username.visibility = View.INVISIBLE
+                    binding.subtitle.visibility = View.INVISIBLE
+                    binding.date.visibility = View.INVISIBLE
+                    binding.order.visibility = View.INVISIBLE
+
+
                 }
 
                 is Result.Error -> {
                     binding.prg.visibility = View.GONE
                     Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                    dialog?.dismiss()
                 }
             }
         }
