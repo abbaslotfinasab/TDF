2ef15473a3e002cb1339af6d561d212b35c9702a
Tohid Shamspour <tohid.shamspour@utechia.com>
2022-03-10 18:18:14 +0330

    remove boilerplate codes!

--- app/src/main/java/com/utechia/tdf/health/HealthParentFragment.kt
+++ app/src/main/java/com/utechia/tdf/health/HealthParentFragment.kt
@@ -37,121 +37,15 @@ class HealthParentFragment : Fragment() {
         binding.pager.adapter = healthViewPagerAdapter
 
         if (arguments != null) {
-            type = requireArguments().getString(OrderEnum.Type.order,"")
+            type = requireArguments().getString(OrderEnum.Type.order, "")
         }
 
-        TabLayoutMediator(binding.tabLayout,binding.pager){ tab, position ->
-
-            when(position){
-
-                0 ->{
-                    tab.text = getText(R.string.daily)
-                }
-
-                1 ->{
-                    tab.text = getText(R.string.weekly)
-                }
-
-                2 ->{
-                    tab.text = getText(R.string.monthly)
-                }
-            }
-
-            when(type){
-
-                OrderEnum.Delivered.order -> {
-
-                    binding.pager.postDelayed({
-                        binding.pager.setCurrentItem(1,true)
-                        binding.tabLayout.selectTab(tab.parent?.getTabAt(1))
-
-                    },100)
-
-                }
-                OrderEnum.Pending.name ->{
-
-                    binding.pager.postDelayed({
-                        binding.pager.setCurrentItem(0,true)
-                        binding.tabLayout.selectTab(tab.parent?.getTabAt(0))
-
-                    },100)
-                }
-
-                OrderEnum.Cancel.name ->{
-
-                    binding.pager.postDelayed({
-                        binding.pager.setCurrentItem(2,true)
-                        binding.tabLayout.selectTab(tab.parent?.getTabAt(2))
-
-                    },100)
-                }
-
-                else -> {
-                    binding.pager.postDelayed({
-                        binding.pager.setCurrentItem(0,true)
-                        binding.tabLayout.selectTab(tab.parent?.getTabAt(0))
-
-                    },100)
-                }
-            }}.attach()
+        val tabTitles =
+            listOf(getText(R.string.daily), getText(R.string.weekly), getText(R.string.monthly))
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
 
 
-        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-
-                when (tab?.position) {
-
-                    0 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    1 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    2 -> {
-                        binding.pager.currentItem = tab.position
-                    }
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-                /*  when(tab?.position){
-
-                    0 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    1 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    2 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                }*/
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-
-                /*   when(tab?.position){
-
-                    0 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    1 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                    2 -> {
-                        binding.pager.currentItem = tab.position
-
-                    }
-                }*/
-            }
-        })
     }
 }
